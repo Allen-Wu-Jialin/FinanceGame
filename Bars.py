4eeb95922a5255cf44a89c004f76c35be07134b4
Allen Wu <49053242+Allen-Wu-Jialin@users.noreply.github.com>
2019-05-04 12:27:47 -0400

    Add files via upload

---
+++ Bars.py
@@ -0,0 +1,13 @@
+#This will create several classes with bars that update over time.
+
+class bar():
+    def __init__(self):
+
+class hunger(bar):
+
+class happiness(bar):
+
+class fatigue(bar):
+
+class fitness(bar):
+    
